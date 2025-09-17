0f95189fe388011118f39a6a2f7dd56d07d41ca0
nick-crowley <ncrowley@beyondtrust.com>
2025-09-17 16:24:28 +0100

    feat: Implement C# app which inserts row into "testdb"

--- simpleC#/HelloWorld/Program.cs
+++ simpleC#/HelloWorld/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Data.SqlClient;
 
-namespace HelloWorld.ConsoleClient
+namespace HelloWorld
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=testdb;User Id=testuser;Password=Qwerty_123;";
+            
+            Console.WriteLine("Opening connection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // 1. Create the table
+                string createTableQuery = @"
+                    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'test_table')
+                    BEGIN
+                        CREATE TABLE test_table (
+                            test_field NVARCHAR(255)
+                        );
+                    END";
+
+                Console.WriteLine("Creating table");
+                using (SqlCommand createCommand = new SqlCommand(createTableQuery, connection))
+                {
+                    createCommand.ExecuteNonQuery();
+                    Console.WriteLine("Table created successfully.");
+                }
+
+                // 2. Insert a row
+                string insertQuery = "INSERT INTO test_table (test_field) VALUES (@value)";
+                Console.WriteLine("Inserting data");
+                using (SqlCommand insertCommand = new SqlCommand(insertQuery, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@value", "Sample text");
+                    insertCommand.ExecuteNonQuery();
+                    Console.WriteLine("Row inserted successfully.");
+                }
+            }
+
         }
     }
 }
